4703d1722dc2c6933e41d90d0aefc68392c2dccf
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2023-05-03 05:18:47 +0600

    server data added and index.js file functionality done

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const chefs = require('./chefs_data.json');
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World! recipe is running')
 })
+
+
+app.get('/chefs', (req, res)=>{
+    res.send(chefs)
+});
+
+app.get('/chefs/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedChef = chefs.find( n=> n.id === id);
+    res.send(selectedChef);
+    // console.log(id);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
