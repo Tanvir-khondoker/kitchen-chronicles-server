9fe392585ad96ac56594e6c9ad7ae6d867a8f026
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2023-05-03 13:44:10 +0600

    cors is added

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const chefs = require('./chefs_data.json');
+const port = 5000;
 
+app.use(cors());
 
+const chefs = require("./chefs_data.json");
 
-app.get('/', (req, res) => {
-  res.send('Hello World! recipe is running')
-})
-
-
-app.get('/chefs', (req, res)=>{
-    res.send(chefs)
+app.get("/", (req, res) => {
+  res.send("Hello World! recipe is running");
 });
 
-app.get('/chefs/:id', (req, res)=>{
-    const id = req.params.id;
-    const selectedChef = chefs.find( n=> n.id === id);
-    res.send(selectedChef);
-    // console.log(id);
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 
-
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChef = chefs.find((n) => n.id === id);
+  res.send(selectedChef);
+  // console.log(id);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-
-
+  console.log(`Example app listening on port ${port}`);
+});
